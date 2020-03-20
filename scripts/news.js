8bd9a49a0f63034e1f0ce18d15dc2203adbbc521
Ng-B <naodg68@gmail.com>
2020-03-20 16:47:35 +0100

    First commit, fonction ajouter_recherche fini

--- scripts/news.js
+++ scripts/news.js
@@ -7,7 +7,28 @@ var recherche_courante_news = [];
 
 
 function ajouter_recherche() {
-	//TODO ...
+    //on recupere la valeur rentree
+    let value_recherche = document.getElementById("zone_saisie").value;
+
+    //on verifie qu'elle n'est pas nulle et qu'elle n'existe pas deja
+    if(value_recherche != null &&  recherches.indexOf(value_recherche) === -1 ){
+        let paragaphe = document.createElement("P");  // on cree un element <p>
+        paragaphe.classList.add("titre-recherche");
+
+        let label = document.createElement("LABEL"); // on cree un element <label>
+        label.innerHTML = value_recherche; // sa valeur devient la chaine saisie par l'utilisateur
+
+        let image = document.createElement("IMG"); // on cree un element <img>
+        image.classList.add("icone-croix");  // on definit sa classe "icone-croix"
+        image.src="img/croix30.jpg";  // on definit l'emplacement de l'image
+
+        paragaphe.appendChild(label);  // on ajoute le label dans le paragraphe de classe "titre-recherche"
+        paragaphe.appendChild(image);  // on ajoute l'image dans le paragraphe de classe "titre-recherche"
+
+        document.getElementById("recherches-stockees").appendChild(paragaphe); // on ajoute le paraphe dans le div d'id "recherches-stockees"
+
+        recherches.push(value_recherche); // on ajoute la chaine saisie dans la valeur glocale
+    }
 }
 
 
